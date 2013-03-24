b5423477330e8a9d6bc736b71479738d2de76bb0
bonfa <francesco.bonfadelli@gmail.com>
2013-03-24 20:54:33 +0100

    inserita la prima parte del progetto. troppo rumore

--- 99-Project/tune_sound.m
+++ 99-Project/tune_sound.m
@@ -22,9 +22,9 @@ function [ ] = tune_sound(recorder,event,fs)
     half_fft = (abs_fft_audio_data(1:N+1));
     
     plot(half_fft);
-    [m,i_max] = max(half_fft);
+    [m,i_max] = max(half_fft(5:length(half_fft)));
     
-    norm_max_freq = i_max*2*1000/fs;
+    norm_max_freq = (i_max+5)*2*1000/fs;
     disp(norm_max_freq);
     
     disp('Play');
