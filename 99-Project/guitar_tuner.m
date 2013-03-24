1074a3fa5460ecdaf5b8ea55a89b45b0d6fa4563
bonfa <francesco.bonfadelli@gmail.com>
2013-03-24 11:36:12 +0100

    inserito lo scheletro per il progetto

---
+++ 99-Project/guitar_tuner.m
@@ -0,0 +1,10 @@
+%{ 
+    This project defines a simple real-time guitar tuner.
+    The main steps of this work are:
+        1) taking as input the sound
+        2) analyzing the main frequence of it
+        3) comparing with the reference frequence and indicate how far it
+        is from it
+
+%}
+
