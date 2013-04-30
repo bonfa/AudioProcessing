f0beef39ee3ca5a331336982ef827961f581fb96
bonfa <francesco.bonfadelli@gmail.com>
2013-04-30 15:34:20 +0200

    inserito il commento per il metodo di pitch detection

--- 99-Project/get_audio_main_frequency_in_guitar_domain.m
+++ 99-Project/get_audio_main_frequency_in_guitar_domain.m
@@ -10,91 +10,77 @@ function [ input_freq ] = get_audio_main_frequency_in_guitar_domain(audio_vector
     N = round(length(fft_abs_vect)/2);
     audio_data_normalized = (fft_abs_vect(1:N+1));
 
-    %create a correct x-axis for the data
-    upper_interval_frequency = round(TunerConstants.FS/2)+1;
-    step = TunerConstants.FS/(2*length(audio_data_normalized));
-    x_axis = 1:step:upper_interval_frequency;
-    x_axis_from_min_freq = x_axis(40:length(x_axis));
-
-    %figure(2);
-    %plot(x_axis(50:length(audio_data_normalized)),audio_data_normalized(50:length(audio_data_normalized)));
-
-
-    %as findpeaks on the whole vector is too expensive, we calculare the
-    %indexes that represent the bound in which looking for the vector.
-
-    min_freq = 40;
-    %max_freq = 400;
-
-    min_i = round(min_freq * 2 * length(audio_data_normalized)/TunerConstants.FS);
-    %max_i = round(max_freq * 2 * length(audio_data_normalized)/TunerConstants.FS);
-
-    %{
-    % version 1 - with findpeaks
-        % find the peaks in the range of defined frequences
-        [~,indexes] = findpeaks(audio_data_normalized(min_i:max_i),'MINPEAKHEIGHT',100,'THRESHOLD',50,'MINPEAKDISTANCE',4);
-
-
-        % normalize indexes
-        normalized_indexes = indexes*(TunerConstants.FS/(2*length(audio_data_normalized)))+min_freq;
-        %disp(normalized_indexes);
-
-        % the frequency is the lowest
-        input_freq = normalized_indexes(1);
-    %}
-    %{
-    % version 2 - with maximum
-    threshold = 30;
-    [m,index] = max(audio_data_normalized(min_i:max_i));
-       
-    if m > threshold
-        normalized_indexes = index*(TunerConstants.FS/(2*length(audio_data_normalized)))+min_freq;
-        input_freq = normalized_indexes;    
-        disp(normalized_indexes);
-    else    
-        input_freq = -1;
-    %}
-        
-    % version 3 - hps METHOD
+      
+    % Pitch detection - hps METHOD (3rd try)
     
-    %todo- allungare il vettore su cui calcolare il sottocampionamento
+    % calculate the scaled versions of the vector downsampled by the factor
+    % 2,3,4 and 5
     fft1 = audio_data_normalized;
     fft2 = downsample(fft1,2);
     fft3 = downsample(fft1,3);
     fft4 = downsample(fft1,4);
     fft5 = downsample(fft1,5);
     
-    % as the vectors have different lengths, i use the length of the
+    % As the vectors have different lengths, i use the length of the
     % shortest one for the product. 
-    % In fact the main frequence is in the common part to all the vectors
+    % As the harmonics in which the energy is concentrated are the overtones, 
+    % a smart way for isolating the f0 (the pitch) is to multiply the scaled
+    % versions of the input downsampled with many factors (1,2,3,4 and 5 here).
+    % With this method the pitch is enhanced. 
     k = length(fft5);
     product = (fft1(1:k).*fft2(1:k).*fft3(1:k).*fft4(1:k).*fft5(1:k));
 
    
     
-    %calculate the max
-    %OSS: in this case the min_i has index 1 --> In order to bring the
-    %right x i have to add min_i
+    
+    % min frequency from which starting the analysis of the vector
+    min_freq = 40;
+    
+    % min_i is the index correspondent to 40 hz in the vector
+    min_i = round(min_freq * 2 * length(audio_data_normalized)/TunerConstants.FS);
+    
+    % Fisrt approximation of the pitch, that is the maximum of the resulting function.
+    %
+    % OSS: as the input is very noisy and the noise is concentrated in the 
+    % low frequencies, the maximum is computated from values greater than 
+    % 40 hz.
     [m,i] = max(product(min_i:length(product)));
     
+    % adjust the index of the vector, adding min_i
     correct_i = i + min_i;
     
-    % Interpolation
+    % As the input sound is sampled with low FS (8 Khz) and the downsampled
+    % versions are even less precise, an operation of interpolation is
+    % introduced in order to improve the precision of the pitch detection.
+    
+    % Interpolation wit spline
+    % not on the whole product function but only around the maximum
+    % (interval of length 10 around the maximum)
+        
     % create the vector that represents the frequency axes for the range
-    % i-1 and i+1 
+    % i-10 and i+10    
     minimum_i = correct_i - 10;
     maximum_i = correct_i + 10;
     
-    %x = %[minimum_i i maximum_i];    
+    % create the vector
     x = minimum_i:maximum_i;
+    % step of the high precision x axis for the interpolating function
     step_finer = 0.001;
+    % x-axis for the interpolating function
     finer_x = minimum_i:step_finer:maximum_i;
+    % part of the function to be interpolated
     y = product(minimum_i:maximum_i);
     
-    % interpolate values i-1,i,i+1
+    % interpolation of the interval around the bad maximum
     interpolated_prod = interp1(x,y,finer_x,'spline');
     
 %{    
+    % debug part - plots the values to be interpolated and the
+    % interpolation function.
+    %
+    % the x-axes does not contain the indexes of the vector but the
+    % frequencies correspondent to the index
+    
     % create the x axis for the interpolated vector
     interp_freq_min = minimum_i*TunerConstants.FS/(2*length(audio_data_normalized));
     interp_freq_max= maximum_i*TunerConstants.FS/(2*length(audio_data_normalized));
@@ -114,17 +100,18 @@ function [ input_freq ] = get_audio_main_frequency_in_guitar_domain(audio_vector
     figure(3);
     plot(product(minimum_i:maximum_i));
 %}    
-    % calculate again the maximum and normalize that frequency
+    
+    % calculate again the maximum - higher precision
     [mi,ii] = max(interpolated_prod);
     
-    
+    % extract the frequency correspondent to the index of the maximum
     interp_index = minimum_i + (ii*(maximum_i-minimum_i)/length(finer_x));
         
-    if mi > 16000
-        
+    % the maximum frequency represents a tone only if the maximum is over a
+    % threshold, otherwise it is noise
+    if mi > 16000    
         input_freq = interp_index*(TunerConstants.FS/(2*length(audio_data_normalized)));%+min_freq;
         %disp(input_freq);
-        
     else    
         input_freq = -1;
     end
